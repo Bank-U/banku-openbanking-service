cfb9ead39e8b2e0276d8f84d7bbfbaa99850ad82
Cristian Batista Herrera <batistaherreracristian@gmail.com>
2025-04-30 20:42:05 +0100

    fix(devops): 🛠️ Sorted config

--- src/main/java/com/banku/openbankingservice/config/KafkaConfig.java
+++ src/main/java/com/banku/openbankingservice/config/KafkaConfig.java
@@ -1,6 +1,15 @@
 package com.banku.openbankingservice.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.apache.kafka.common.config.SaslConfigs;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
@@ -19,6 +28,15 @@ import org.springframework.kafka.config.TopicBuilder;
 @Configuration
 public class KafkaConfig {
 
+    @Value("${spring.kafka.properties.security.protocol}")
+    private String securityProtocol;
+    
+    @Value("${spring.kafka.properties.sasl.mechanism}")
+    private String saslMechanism;
+    
+    @Value("${spring.kafka.properties.sasl.jaas.config}")
+    private String jaasConfig;
+
     @Bean
     public NewTopic openbankingTopic() {
         return TopicBuilder.name("banku.openbanking")
@@ -26,4 +44,24 @@ public class KafkaConfig {
                 .replicas(1)
                 .build();
     }
+    
+    @Bean
+    public Map<String, Object> producerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, securityProtocol);
+        props.put(SaslConfigs.SASL_MECHANISM, saslMechanism);
+        props.put(SaslConfigs.SASL_JAAS_CONFIG, jaasConfig);
+        return props;
+    }
+    
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+    
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
 } 
